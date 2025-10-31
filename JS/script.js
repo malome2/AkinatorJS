ab816144941bea2f0607dd928de61dbac4c242c7
Marcos <malome2@jviladoms.cat>
2025-10-31 10:33:05 +0100

    JSON + JS preguntas + Index temp

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,46 @@
-// aaaa
+const preguntas = [
+    { key: "queso", text: "¿Tu producto tiene queso?" },
+    { key: "postre", text: "¿Tu producto es un postre?" }, 
+    { key: "helado", text: "¿Tu producto es un helado?" }, 
+    { key: "cuchara", text: "¿Tu producto se sirve con cuchara?" }, 
+    { key: "toping", text: "¿Tu producto tiene topping?" }, 
+    { key: "pistacho", text: "¿Tu producto contiene pistacho?" }, 
+    { key: "oreo", text: "¿Tu producto contiene Oreo?" }, 
+    { key: "chocolateBlanco", text: "¿Tu producto contiene chocolate blanco?" }, 
+    { key: "caramelo", text: "¿Tu producto contiene caramelo?" }, 
+    { key: "kitKat", text: "¿Tu producto contiene KitKat?" }, 
+    { key: "manzana", text: "¿Tu producto contiene manzana?" }, 
+    { key: "caliente", text: "¿Tu producto se sirve caliente?" }, 
+    { key: "niños", text: "¿Tu producto es del menú infantil?" }, 
+    { key: "desayuno", text: "¿Tu producto es un desayuno?" }, 
+    { key: "chocolate", text: "¿Tu producto contiene chocolate?" }, 
+    { key: "galleta", text: "¿Tu producto es una galleta?" }, 
+    { key: "hamburgesa", text: "¿Tu producto es una hamburguesa?" }, 
+    { key: "bacon", text: "¿Tu producto contiene bacon?" }, 
+    { key: "burrito", text: "¿Tu producto es un burrito?" }, 
+    { key: "barbacoa", text: "¿Tu producto contiene salsa barbacoa?" }, 
+    { key: "tomate", text: "¿Tu producto contiene tomate?" }, 
+    { key: "lechuga", text: "¿Tu producto contiene lechuga?" }, 
+    { key: "vaca", text: "¿Tu producto contiene carne de vaca?" }, 
+    { key: "doble", text: "¿Tu producto tiene doble de carne?" }, 
+    { key: "cebolla", text: "¿Tu producto contiene cebolla?" }, 
+    { key: "pescado", text: "¿Tu producto contiene pescado?" }, 
+    { key: "complemento", text: "¿Tu producto es un complemento?" }, 
+    { key: "patata", text: "¿Tu producto contiene patata?" }, 
+    { key: "corteFino", text: "¿Tu producto tiene corte fino?" }, 
+    { key: "picante", text: "¿Tu producto es picante?" }, 
+    { key: "hueso", text: "¿Tu producto contiene hueso?" }, 
+    { key: "rebozado", text: "¿Tu producto está rebozado?" }, 
+    { key: "alcohol", text: "¿Tu producto contiene alcohol?" }, 
+    { key: "agua", text: "¿Tu producto es agua?" }, 
+    { key: "gas", text: "¿Tu producto es con gas?" }, 
+    { key: "zumo", text: "¿Tu producto es un zumo?" }, 
+    { key: "naranja", text: "¿Tu producto contiene naranja?" }, 
+    { key: "citrico", text: "¿Tu producto es cítrico?" }, 
+    { key: "fanta", text: "¿Tu producto es Fanta?" }
+];
+
+
+function cambiarPregunta(){
+
+}
